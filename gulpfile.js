ce94f66bb24d3b37eb63c337a2608e2b4b7d9273
Igor Kryvenko <kryvenko7igor@gmail.com>
2018-08-05 11:47:39 +0300

    Add bower. Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,39 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs'),
+    cssnano = require('gulp-cssnano'),
+    rename =require('gulp-rename');
 
 gulp.task('sass', function() {
   return gulp.src('app/sass/**/*.+(scss|sass)')
     .pipe(sass())
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('app/css/libs.css'))
     .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
+gulp.task('scripts', function() {
+  return gulp.src([
+    'app/libs/jquery/dist/jquery.min.js',
+    'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+  ])
+  .pipe(concat('libs.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('app/js'));
+});
+
+gulp.taks('minify-css',['sass'] function() {
+  return gulp.src('app/css/libs.css')
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch', ['browser-sync', 'minify-css', 'scripts'], function() {
   gulp.watch('app/sass/**/*.+(scss|sass)', ['sass']);
+  gulp.watch('app/**/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.html', browserSync.reload);
 });
 
 gulp.task('browser-sync', function() {
@@ -20,4 +43,4 @@ gulp.task('browser-sync', function() {
     },
     notify: false
   })
-})
+});
